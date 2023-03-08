7dd89cb0266ecf616241833a498c5e21374353c7
JeyJr <jeyjr.dg@gmail.com>
2023-03-08 11:24:53 -0300

    Feat: Asteroids Status e Movimentacao

--- Assets/Scripts/Interfaces/IStartSpawnObj.cs
+++ Assets/Scripts/Interfaces/IStartSpawnObj.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-
-public interface IStartSpawnObj
+﻿public interface IStartSpawnObj
 {
     public void StartSpawnObj();
 }
--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
